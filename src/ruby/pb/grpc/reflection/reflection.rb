2f127fa75d96f313fd4a709dd2a1270b3448f715
Gearnode <bryan@frimin.fr>
2019-12-14 20:15:55 +0100

    save wip

--- src/ruby/pb/grpc/reflection/reflection.rb
+++ src/ruby/pb/grpc/reflection/reflection.rb
@@ -1,3 +1,8 @@
+# REQUIRED FOR Google::Protobuf::FileDescriptorProto
+require 'protobuf'
+require 'google/protobuf/descriptor.pb'
+
+
 require 'grpc'
 require 'grpc/reflection/v1alpha/reflection_services_pb'
 require 'grpc/errors'
@@ -10,11 +15,38 @@ module Grpc
       end
 
       def server_reflection_info(requests)
-        # binding.irb
         q = EnumeratorQueue.new(self)
         t = Thread.new do
           requests.each do |req|
             case req.message_request
+            when :file_containing_symbol
+              service_name = req.file_containing_symbol
+              services = @server.send(:rpc_handlers).map { |_, h| h.owner }.uniq
+              service = services.find { |service| service.service_name == service_name }
+              if service.nil?
+                resp = V1alpha::ServerReflectionResponse.new(
+                  valid_host: req.host,
+                  original_request: req,
+                  error_response: V1alpha::ErrorResponse.new(
+                    error_code: 5, # NOT FOUND CODE
+                    error_message: "Symbol not found: #{service_name}",
+                  )
+                )
+                q.push(resp)
+              else
+                Google::Protobuf::FileDescriptorProto
+                resp = V1alpha::ServerReflectionResponse.new(
+                  valid_host: req.host,
+                  original_request: req,
+                  file_descriptor_response: V1alpha::FileDescriptorResponse.new(
+                    file_descriptor_proto: ['ServerReflectionInfo'] #service.rpc_descs.map { |method, _| method.to_s }
+                  )
+                )
+                q.push(resp)
+                puts "#" * 70
+                puts resp
+                puts "#" * 70
+              end
             when :list_services
               resp = V1alpha::ServerReflectionResponse.new(
                 valid_host: req.host,
@@ -36,29 +68,6 @@ module Grpc
         end
         t.priority = -2
         q.each_item
-
-        
-       # out = V1alpha::ServerReflectionResponse.new
-        #call.each_remote_read do |req|
-        #  out.valid_host = req.host
-        #  out.original_request = req
-
-        #  case req.message_request
-        #  when :file_by_filename
-        #    puts "file by name request"
-        #  when :file_containing_symbol
-        #    puts "file containing symbol request"
-        #  when :file_containing_extension
-        #    puts "file containing extension request"
-        #  when :all_extension_numbers_of_type
-        #    puts "all extension numbers of type request"
-        #  when :list_services
-         #   puts "list services request"
-        #  else
-        #    raise GRPC::InvalidArgument, "invalid message request"
-        #  end
-       # end
-       # out
       end
     end
   end
